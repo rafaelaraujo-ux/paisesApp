f84ce0004c4e19eab89d1646d69f541092a986cc
rafaelaraujo-ux <rduento@gmail.com>
2021-03-30 09:36:29 +0200

    add RouterModule

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { PorContinenteComponent } from './pais/pages/por-continente/por-continente.component';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PorPaisComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'continente',
+    component: PorContinenteComponent,
+  },
+  {
+    path: 'capital',
+    component: PorCapitalComponent,
+  },
+  {
+    path: 'pais/:id',
+    component: VerPaisComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
